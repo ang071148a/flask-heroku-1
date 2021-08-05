33491b4c502cd08ede385e4bcac1395a29c64856
ang071148 <87840767+ang071148@users.noreply.github.com>
2021-08-05 21:49:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def get_api():
 
 @app.route('/name')
 def name():
-    return “สัณหพร รัตนพาหิระ“
+    return “สัณหพร รัตนพาหิระ เลขที่ 26 ม.4/10“
 
 if __name__ == "__main__":
     app.run(debug=False)
